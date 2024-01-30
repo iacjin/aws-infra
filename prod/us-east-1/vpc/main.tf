8663166f63786bb025ed15f886c4f15d1dc0fe22
Jin Chen <jin.chen@yitmh.com>
2024-01-30 16:32:32 +0800

    change source

--- prod/us-east-1/vpc/main.tf
+++ prod/us-east-1/vpc/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "vpc" {
-  source      = "app.terraform.io/jurassic-park/vpc/aws"
+  source      = "app.terraform.io/pokemoncenter/vpc/aws"
   version     = "1.0.0"
   vpc_configs = var.vpc_configs
 }
